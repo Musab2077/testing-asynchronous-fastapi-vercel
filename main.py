9dac3d7f96e54ecb5b766ed0a8950cf37c053cc5
Musab2077 <muhammadmusab2077@gmail.com>
2025-09-07 15:51:25 +0500

    testing hugging face embeddings

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-app = FastAPI()
+app = FastAPI(title="Hugging Face Embeddings API")
 
-@app.get('/')
+@app.on_event("startup")
+async def load_model():
+    global embeddings
+    embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
+    embeddings=HuggingFaceEmbeddings(model_name= embedding_model_name)
+
+@app.get('/testing')
 async def testing():
     try:
-        embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
-        embeddings=HuggingFaceEmbeddings(model_name= embedding_model_name)
         return "hello"
     except Exception as e:
         return e
