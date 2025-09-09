480ec7d873e7bef056d6cf8b22a5d42212baf1db
Musab2077 <muhammadmusab2077@gmail.com>
2025-09-09 19:58:30 +0500

    is sentence tranformers library crashing the vercel server

--- main.py
+++ main.py
@@ -14,42 +14,42 @@ load_dotenv()
 
 app = FastAPI(title="Hugging Face Embeddings API")
 
-embeddings=HuggingFaceEmbeddings(model_name= "thenlper/gte-tiny")
-# embeddings = HuggingFaceInferenceAPIEmbed 
+# embeddings=HuggingFaceEmbeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2")
+# # embeddings = HuggingFaceInferenceAPIEmbed 
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+# GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-index_name = "midster-bot"
+# index_name = "midster-bot"
 
-documents = PineconeVectorStore.from_existing_index(
-    index_name=index_name,
-    embedding=embeddings
-)
-retriever = documents.as_retriever(search_type = 'similarity', search_kwargs = {'k':3})
-llm = ChatGroq(model_name = 'llama-3.3-70b-versatile',groq_api_key = GROQ_API_KEY)
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. keep the "
-    "answer detailed."
-    "Give the answers in markdown format"
-    "\n\n"
-    "{context}"
-)
+# documents = PineconeVectorStore.from_existing_index(
+#     index_name=index_name,
+#     embedding=embeddings
+# )
+# retriever = documents.as_retriever(search_type = 'similarity', search_kwargs = {'k':3})
+# llm = ChatGroq(model_name = 'llama-3.3-70b-versatile',groq_api_key = GROQ_API_KEY)
+# system_prompt = (
+#     "You are an assistant for question-answering tasks. "
+#     "Use the following pieces of retrieved context to answer "
+#     "the question. If you don't know the answer, say that you "
+#     "don't know. keep the "
+#     "answer detailed."
+#     "Give the answers in markdown format"
+#     "\n\n"
+#     "{context}"
+# )
 
-system_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+# system_prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", system_prompt),
+#         ("human", "{input}"),
+#     ]
+# )
 
-question_answer_chain = create_stuff_documents_chain(llm, system_prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+# question_answer_chain = create_stuff_documents_chain(llm, system_prompt)
+# rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
 @app.get('/test/{text}')
 async def test_embedding(text: str):
-    response = rag_chain.invoke({"input" : text})
-    return response
+    # response = rag_chain.invoke({"input" : text})
+    return {"your text is this" : text}
